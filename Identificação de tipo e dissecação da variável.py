41da89fdddd5dee48ed9638e583e52848b1997da
PabloVpy <rumoita.estudos@gmail.com>
2026-07-05 16:46:06 -0300

    Identificação de tipo e dissecação da variável.py

---
+++ Identificação de tipo e dissecação da variável.py
@@ -0,0 +1,20 @@
+n = input('Digite algo para identificação:')
+print(type(n))
+
+print('Ele é numérico?')
+print(n.isnumeric())
+
+print('Ele é decimal?')
+print(n.isdecimal())
+
+print('Ele é letra?')
+print(n.isalpha())
+
+print('Ele é alfanumérico?')
+print(n.isalnum())
+
+print('Ele é maiúsculo?')
+print(n.isupper())
+
+print('Ele é minúsculo?')
+print(n.islower())
