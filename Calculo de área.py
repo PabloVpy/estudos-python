d5fd37f55ac178d041c129ce36d38e4c4f4e1ec5
PabloVpy <rumoita.estudos@gmail.com>
2026-07-06 21:25:26 -0300

    Calculo de área.py

---
+++ Calculo de área.py
@@ -0,0 +1,5 @@
+n1 = float(input('Me diga a largura da parede:'))
+n2 = float(input('Me diga a altura da parede:'))
+a = n1 * n2
+l = a / 2
+print(f'A área da parede é {a}m², ou seja vai ser usado {l}l de tinta. ')
